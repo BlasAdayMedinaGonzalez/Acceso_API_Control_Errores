5f723997d09b505b838be88148f897ddb7d7463f
BlasAdayMedinaGonzalez <usr2702@salesianos-lacuesta.net>
2021-12-03 02:41:53 +0000

    Created project Api-access hero with:
    feat: Access all heroes
    
    With the word all, you can access all heroes from the api

---
+++ app/src/main/java/com/digitalcataway/accesssuperheroapi/MainActivity.java
@@ -0,0 +1,89 @@
+package com.digitalcataway.accesssuperheroapi;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.digitalcataway.accesssuperheroapi.utils.NetworkUtils;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    EditText searchBox;
+    TextView urlDisplay;
+    TextView searchResults;
+
+    public class GithubQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String gitHubSearchResults = null;
+
+            try {
+                gitHubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return gitHubSearchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null && !s.equals("")){
+                searchResults.setText(s);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.launch) {
+
+            URL apiURL = null;
+            try {
+                apiURL = NetworkUtils.buildUrl(searchBox.getText().toString());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            urlDisplay.setText(apiURL.toString());
+
+            new GithubQueryTask().execute(apiURL);
+            /*try {
+                String response = NetworkUtils.getResponseFromHttpUrl(apiURL);
+                Log.i("MainActivity", response);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }*/
+        }
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        searchBox = (EditText) findViewById(R.id.search_box);
+        urlDisplay = (TextView) findViewById(R.id.url_display);
+        searchResults = (TextView) findViewById(R.id.api_results);
+    }
+}
---
+++ app/src/main/java/com/digitalcataway/accesssuperheroapi/utils/NetworkUtils.java
@@ -0,0 +1,45 @@
+package com.digitalcataway.accesssuperheroapi.utils;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+    final static String API_BASE_URL_ALL = "https://akabab.github.io/superhero-api/api";
+
+    public static URL buildUrl(String apiSearchQuery) throws MalformedURLException {
+
+        Uri builUri = Uri.parse(API_BASE_URL_ALL).buildUpon()
+                .appendPath(apiSearchQuery + ".json")
+                .build();
+        URL url = null;
+        url = new URL(builUri.toString());
+        return url;
+    }
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection =  (HttpURLConnection) url.openConnection();
+
+        InputStream in = urlConnection.getInputStream();
+
+        Scanner sc = new Scanner(in);
+        sc.useDelimiter("\\A");
+
+        try {
+            boolean hasInput = sc.hasNext();
+
+            if (hasInput) {
+                return sc.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+
+    }
+}
